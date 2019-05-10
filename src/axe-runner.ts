1218fb51f72b69d41df0998511edabda61a17e98
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-10 11:25:52 -0700

    Properly stringifying function to call on the webpage

--- src/axe-runner.ts
+++ src/axe-runner.ts
@@ -1,18 +1,20 @@
 import { source } from 'axe-core';
-// import * as Axe from 'axe-core';
+import * as Axe from 'axe-core';
 
-// declare const axe: typeof Axe;
+declare const axe: typeof Axe;
 
 export const inject = (webContents: Electron.WebContents) => {
     webContents.executeJavaScript(source);
 };
 
 export const run = (webContents: Electron.WebContents) => {
-    const source = `
-    axe.run().then(results => {
-        console.log(results.violations);
-    });
-    `;
+    const source = `(${String(scan)})()`;
 
     webContents.executeJavaScript(source);
 };
+
+const scan = () => {
+    axe.run().then(results => {
+        console.log(results.violations);
+    });
+}
